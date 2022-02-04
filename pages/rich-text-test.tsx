07f88f6b8e6c355a3569acbbb520df0f41400cc8
JNaftali <jmarantz@thelabnyc.com>
2022-02-03 22:15:27 -0500

    vanity I guess

--- pages/rich-text-test.tsx
+++ pages/rich-text-test.tsx
@@ -1,14 +1,16 @@
-import { GetServerSidePropsResult } from 'next';
+import { GetServerSidePropsContext, GetServerSidePropsResult } from 'next';
 import RichText from '../components/RichText';
 
 export default function Page({ wagtail }: { wagtail: any }) {
   return <RichText data={wagtail.text} />;
 }
 
-export async function getServerSideProps(): Promise<
-  GetServerSidePropsResult<any>
-> {
-  const result = await fetch('http://localhost:8000/api/v2/pages/3/');
+export async function getServerSideProps(
+  context: GetServerSidePropsContext,
+): Promise<GetServerSidePropsResult<any>> {
+  const result = await fetch(
+    `http://localhost:8000/api/v2/pages/find/?html_path=${context.resolvedUrl}`,
+  );
   const wagtail = await result.json();
 
   return {
